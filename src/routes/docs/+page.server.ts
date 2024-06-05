944e573443b19c9ca62b03da281ad578405c3276
highonhopium <jks.hardy@gmail.com>
2024-06-05 19:37:28 +0100

    relative redirect

--- src/routes/docs/+page.server.ts
+++ src/routes/docs/+page.server.ts
@@ -8,7 +8,7 @@ export const load: PageLoad = async (event) => {
 	const { categorisedArticles } = await parent();
 
 	const _mainSlug = categorisedArticles?.[0]?.articles?.[0]?.slug;
-	if (_mainSlug) throw redirect(307, `/docs/${_mainSlug}`);
+	if (_mainSlug) throw redirect(307, `docs/${_mainSlug}`);
 
 	// If there is not route to the docs, then throw the error
 	throw error(500, 'Something went wrong :(');
