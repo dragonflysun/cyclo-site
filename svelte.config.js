47bece613bd5af5f6edd404efcca8e16d1a33916
highonhopium <jks.hardy@gmail.com>
2024-06-05 19:11:38 +0100

    path

--- svelte.config.js
+++ svelte.config.js
@@ -24,9 +24,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		// paths: {
+		// 	base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		// }
 	},
 
 	extensions: ['.svelte', '.svx', '.md']
