257414646c73d26c6239d1c24c9aa3956c5e09a5
highonhopium <jks.hardy@gmail.com>
2024-06-05 15:05:53 +0100

    setting up vitest and svelte testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import { svelteTesting } from '@testing-library/svelte/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), svelteTesting()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		includeSource: ['src/**/*.{js,ts}'],
+		setupFiles: ['./vitest-setup.ts']
 	}
 });
