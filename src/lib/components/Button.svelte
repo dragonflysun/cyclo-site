36ba82226d82d097bb2d033f17fa6b99e667331e
Jamie Harding <jharding142@icloud.com>
2024-06-13 17:19:54 +0200

    full-h max button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,21 +1,14 @@
 <script lang="ts">
 	export let inset: boolean = false;
-	export let customClass: string = '';
 	$: outset = inset === false;
 </script>
 
 <button
 	{...$$restProps}
-	class={`flex h-fit items-center justify-center gap-2 border-4 border-white bg-primary px-4 py-2 font-bold   ${customClass}`}
+	class={`flex h-fit items-center justify-center gap-2 border-4 border-white bg-primary px-4 py-2 font-bold text-white`}
 	on:click
 	class:inset
 	class:outset
 >
 	<slot></slot></button
 >
-
-<!-- <style lang="postcss">
-	button {
-		@apply flex h-fit items-center justify-center gap-2 border-4 border-white bg-primary px-4 py-2 font-bold text-white;
-	}
-</style> -->
