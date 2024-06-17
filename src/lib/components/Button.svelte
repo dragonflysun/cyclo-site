fc0f460d08c65651c258c249328c9da62ef75171
highonhopium <jks.hardy@gmail.com>
2024-06-17 15:59:17 +0100

    disabled and hover styles for button

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -2,11 +2,11 @@
 	export let inset: boolean = false;
 </script>
 
-<button {...$$restProps} on:click class:inset class:outset={!inset}> <slot></slot></button>
+<button {...$$restProps} on:click class:inset class:outset={!inset}><slot></slot></button>
 
 <style lang="postcss">
 	button {
-		@apply flex h-fit items-center justify-center gap-2 border-4 border-white bg-primary px-4 py-2 font-bold text-white;
+		@apply flex items-center justify-center gap-2 border-4 border-white bg-primary px-4 py-2 font-bold text-white transition-all hover:bg-blue-700 disabled:bg-neutral-600;
 	}
 
 	.outset {
@@ -15,5 +15,10 @@
 
 	.inset {
 		border-style: inset;
+	}
+
+	button:disabled {
+		@apply cursor-not-allowed;
+		border-style: solid;
 	}
 </style>
