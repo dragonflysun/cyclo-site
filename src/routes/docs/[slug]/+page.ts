03448fd345e1c0a4aa37f5642951aa0b9263560e
Jamie Harding <jharding142@icloud.com>
2024-07-05 08:34:26 +0100

    fix type assertion

--- src/routes/docs/[slug]/+page.ts
+++ src/routes/docs/[slug]/+page.ts
@@ -6,7 +6,7 @@ export const load = async ({ params }) => {
 	const basePath = base;
 	const modules = import.meta.glob(`/src/docs/**/*.{md,svx,svelte.md}`);
 
-	let match = {};
+	let match: { path?: string; resolver?: App.MdsvexResolver } = {};
 
 	for (const [path, resolver] of Object.entries(modules)) {
 		const adjustedPath = `${basePath}${path.replace('/src/docs', '')}`;
@@ -19,7 +19,6 @@ export const load = async ({ params }) => {
 	const post = await match?.resolver?.();
 
 	if (!post || !post.metadata?.published) {
-		console.log('NO POST!');
 		throw error(404); // Couldn't resolve the post
 	}
 
