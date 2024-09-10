d0caf7a37ec8d0a52ba47befd078b5d9d46874b7
Jamie Harding <jharding142@icloud.com>
2024-09-10 09:57:53 +0200

    add setup

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -1,1 +1,10 @@
 import '@testing-library/jest-dom/vitest';
+import { vi } from 'vitest';
+const { mockWagmiConfigStore } = await vi.hoisted(() => import('./src/lib/mocks/mockStores'));
+
+vi.mock('svelte-wagmi', async (importOriginal) => {
+	return {
+		...((await importOriginal()) as object),
+		wagmiConfig: mockWagmiConfigStore
+	};
+});
