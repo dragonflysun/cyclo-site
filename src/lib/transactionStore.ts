d7f08dc44f3058bf57b5fde95a142a232c115fbe
Jamie Harding <jharding142@icloud.com>
2024-09-25 13:48:33 +0200

    no errors

--- src/lib/transactionStore.ts
+++ src/lib/transactionStore.ts
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
-import type { Hex, WaitForTransactionReceiptErrorType, WriteContractErrorType } from 'viem';
+import type { Hex, WaitForTransactionReceiptErrorType } from 'viem';
 
 import type { Config } from '@wagmi/core';
 import { waitForTransactionReceipt } from '@wagmi/core';
