70d7fcad526ca19dd1644ab271ce2897c3aea630
Jamie Harding <jharding142@icloud.com>
2024-11-04 22:40:24 +0100

    new deploy

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from '@wagmi/cli';
 import { foundry } from '@wagmi/cli/plugins';
 import { actions } from '@wagmi/cli/plugins';
-import { type ActionsConfig } from '@wagmi/cli/plugins';
 import { erc20Abi, type Abi } from 'viem';
 import { erc1155Abi } from './src/lib/contracts/erc1155Abi';
 export default defineConfig({
@@ -20,7 +19,7 @@ export default defineConfig({
 		actions(),
 		foundry({
 			include: ['ERC20PriceOracleReceiptVault.json'],
-			project: './ethgild'
+			project: './cyclo.sol'
 		})
 	]
 });
