b96feb4f81c17e44dd3bd6c598931bca382ff26c
highonhopium <jks.hardy@gmail.com>
2024-12-06 09:06:24 +0000

    getting data from blockscout

--- src/lib/queries/getReceipts.ts
+++ src/lib/queries/getReceipts.ts
@@ -5,9 +5,16 @@ import { readErc1155BalanceOf } from '../../generated';
 import type { Config } from '@wagmi/core';
 import type { Hex } from 'viem';
 
-export const getReceipts = async (address: string, erc1155Address: string, config: Config) => {
-	const query: string = `https://api.routescan.io/v2/network/mainnet/evm/14/address/${address}/erc1155-holdings?limit=1000`;
+type BlockScoutData = {
+	token: {
+		address: string;
+	};
+	value: string;
+	id: string;
+};
 
+export const getReceipts = async (address: string, erc1155Address: string, config: Config) => {
+	const query: string = `https://flare-explorer.flare.network/api/v2/addresses/${address}/nft?type=ERC-1155`;
 	const getBalance = async (tokenId: bigint) => {
 		const res = await readErc1155BalanceOf(config, {
 			address: erc1155Address as Hex,
@@ -19,8 +26,12 @@ export const getReceipts = async (address: string, erc1155Address: string, confi
 	try {
 		let data: Receipt[] = [];
 		const response = await axios.get(query);
-
-		data = response.data.items.filter((item: Receipt) => item.tokenAddress === erc1155Address);
+		data = response.data.items.map((item: BlockScoutData) => ({
+			tokenId: item.id,
+			balance: item.value,
+			tokenAddress: item.token.address
+		}));
+		data = data.filter((item: Receipt) => item.tokenAddress === erc1155Address);
 		data = data.map((item: Receipt) => ({
 			...item,
 			readableTokenId: formatEther(item.tokenId)
--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -5,9 +5,9 @@ import type { Hex } from 'viem';
 import type { Receipt } from './types';
 import { flare } from '@wagmi/core/chains';
 
-export const cysFlrAddress = writable<Hex>('0x141Fe89F0F43274853c54A918904Af59A944D633');
+export const cysFlrAddress = writable<Hex>('0xD4B47E8F79a65a6376856401efDe8334E00af738');
 export const sFlrAddress = writable<Hex>('0x12e605bc104e93B45e1aD99F9e555f659051c2BB');
-export const erc1155Address = writable<Hex>('0x5C1Be3016d37CE723aC33b2ddbdc49b95337e172');
+export const erc1155Address = writable<Hex>('0xB306F9FC8555c77FCFDb59b8F8901E97B5F02DB4');
 
 export const targetNetwork = writable<Chain>(flare);
 export const wrongNetwork = derived(
