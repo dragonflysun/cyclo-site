e463536beaf69437dc01f45afe94e16d968eae92
highonhopium <jks.hardy@gmail.com>
2024-12-07 20:36:29 +0000

    addresses

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -5,9 +5,9 @@ import type { Hex } from 'viem';
 import type { Receipt } from './types';
 import { flare } from '@wagmi/core/chains';
 
-export const cysFlrAddress = writable<Hex>('0xD4B47E8F79a65a6376856401efDe8334E00af738');
+export const cysFlrAddress = writable<Hex>('0x6Eb062Ba625ccafeD34450d80f24Ba11bcef374C');
 export const sFlrAddress = writable<Hex>('0x12e605bc104e93B45e1aD99F9e555f659051c2BB');
-export const erc1155Address = writable<Hex>('0xB306F9FC8555c77FCFDb59b8F8901E97B5F02DB4');
+export const erc1155Address = writable<Hex>('0x6A512b5cD7DF91fA8193E89BD8BD5264519BbF85');
 
 export const targetNetwork = writable<Chain>(flare);
 export const wrongNetwork = derived(
