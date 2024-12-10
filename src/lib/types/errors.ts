b8ea5a2e87b940f1a4c8dbc0d16e78a8e6abdee0
Jamie Harding <jharding142@icloud.com>
2024-12-10 11:43:24 +0100

    fx typos

--- src/lib/types/errors.ts
+++ src/lib/types/errors.ts
@@ -1,11 +1,11 @@
 export enum TransactionErrorMessage {
-	USER_REJECTED_APPROVAL = 'The approval transaction was rejected by the user. ',
-	USER_REJECTED_LOCK = 'The lock transaction was rejected by the user. ',
-	USER_REJECTED_UNLOCK = 'The unlock transaction was rejected by the user. ',
-	LOCK_FAILED = 'There was an error in the lock transaction, and it failed to lock your SFLR. Please see the block explorer link for more information.  ',
-	UNLOCK_FAILED = 'There was an error unlocking your sFLR. Please see the block explorer link for more information. ',
+	USER_REJECTED_APPROVAL = 'The approval transaction was rejected by the user.',
+	USER_REJECTED_LOCK = 'The lock transaction was rejected by the user.',
+	USER_REJECTED_UNLOCK = 'The unlock transaction was rejected by the user.',
+	LOCK_FAILED = 'There was an error in the lock transaction, and it failed to lock your sFLR. Please see the block explorer link for more information.',
+	UNLOCK_FAILED = 'There was an error unlocking your sFLR. Please see the block explorer link for more information.',
 	APPROVAL_FAILED = 'There was an error in the approval transaction, and it failed to approve the cysFLR spend. Please see the block explorer link for more information.',
-	TIMEOUT = 'Transaction timed out... Your transaction may still succeed - please see the block exporer link for more information.',
+	TIMEOUT = 'Transaction timed out... Your transaction may still succeed - please see the block explorer link for more information.',
 	BALANCE_REFRESH_FAILED = 'There was an error refreshing your balances. This does not mean that the transaction was unsuccessful. Please see the block explorer link for more information.',
-	GENERIC = 'Something went wrong'
+	GENERIC = 'Something went wrong.'
 }
