c9d36ed65c502dc04a69208e8de4ac35eb53b71c
Jamie Harding <jharding142@icloud.com>
2024-12-10 18:30:55 +0100

    fix any

--- src/lib/utils/handleDecimalSeparator.ts
+++ src/lib/utils/handleDecimalSeparator.ts
@@ -1,4 +1,4 @@
-export const handleDecimalSeparator = (e) => {
+export const handleDecimalSeparator = (e: { target: { value: string } }) => {
 	const value = e.target.value;
 	const sanitizedValue = value.replace(/[,.]/, '.').replace(/\.(?=.*\.)/g, '');
 	const finalValue = sanitizedValue.replace(/[^\d.]/g, '');
