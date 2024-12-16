1d5a334791d9647cf7e5295664f0c3fb82407c82
Jamie Harding <jharding142@icloud.com>
2024-12-16 13:26:24 +0100

    main

--- src/lib/components/Input.test.ts
+++ src/lib/components/Input.test.ts
@@ -3,15 +3,15 @@ import Input from './Input.svelte';
 import { describe, it, expect, vi } from 'vitest';
 
 describe('Input', () => {
-	it('renders the input field, token name, and MAX button', () => {
-		render(Input, { tokenName: 'FLR' });
+	it('renders the input field and unit', () => {
+		render(Input, { amount: '0.0', unit: 'FLR' });
 
 		const input = screen.getByPlaceholderText('0.0');
-		const tokenName = screen.getByText('FLR');
+		const unit = screen.getByText('FLR');
 		const maxButton = screen.getByText('MAX');
 
 		expect(input).toBeInTheDocument();
-		expect(tokenName).toBeInTheDocument();
+		expect(unit).toBeInTheDocument();
 		expect(maxButton).toBeInTheDocument();
 	});
 
@@ -106,36 +106,5 @@ describe('Input', () => {
 		await fireEvent.click(maxButton);
 
 		expect(mockDispatch).toHaveBeenCalled();
-	});
-
-	it('sets input value to maxValue when MAX button is clicked', async () => {
-		mockSignerAddressStore.mockSetSubscribeValue('0x');
-
-		const maxValue = 50;
-		render(Input, { maxValue, tokenName: 'FLR' });
-
-		const maxButton = screen.getByText('MAX');
-		await fireEvent.click(maxButton);
-
-		const input = screen.getByPlaceholderText('0.0');
-		expect((input as HTMLInputElement).value).toBe(maxValue.toString());
-	});
-
-	it('disables the MAX button when signerAddress is not set', () => {
-		mockSignerAddressStore.mockSetSubscribeValue('');
-
-		render(Input, { tokenName: 'FLR' });
-
-		const maxButton = screen.getByText('MAX');
-		expect(maxButton.hasAttribute('disabled')).toBeTruthy();
-	});
-
-	it('enables the MAX button when signerAddress is set', async () => {
-		mockSignerAddressStore.mockSetSubscribeValue('0x');
-
-		render(Input, { tokenName: 'FLR' });
-
-		const maxButton = screen.getByText('MAX');
-		expect(maxButton.getAttribute('disabled')).toBeFalsy();
 	});
 });
