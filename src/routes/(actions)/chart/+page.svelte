fe49f3b199fd7648728327413300f86c153703bc
Jamie Harding <jharding142@icloud.com>
2024-12-16 14:48:01 +0100

    add chart

---
+++ src/routes/(actions)/chart/+page.svelte
@@ -0,0 +1,11 @@
+<style>
+	#dexscreener-embed {
+		position: relative;
+		width: 100%;
+		padding-bottom: 125%;
+	}
+</style>
+
+<div id="dexscreener-embed">
+	<iframe title="chart" class='w-full h-[80vh]' src="https://dexscreener.com/flare/0x2dfbDB28013D7c7A93ECFf227C3B20bb3050BC2e?embed=1&loadChartSettings=0&trades=0&tabs=0&info=0&chartLeftToolbar=0&chartDefaultOnMobile=1&chartTheme=dark&theme=dark&chartStyle=0&chartType=usd&interval=15"></iframe>
+</div>
